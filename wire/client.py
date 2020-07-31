fdc5087f8aeab626c0f28eaa3c63b6b0b6204a89
iamnotedible <hehonglu123@126.com>
2020-07-31 03:11:22 -0400

    normal mode

--- wire/client.py
+++ wire/client.py
@@ -4,15 +4,23 @@ import time, traceback, sys
 url='rr+tcp://localhost:52222/?service=Create'
 #take url from command line
 if (len(sys.argv)>=2):
-    url=sys.argv[1]
+		url=sys.argv[1]
+
+###2 modes available, choose either one		
+########wire connection mode:
+# obj=RRN.ConnectService(url)
+# turtle_change=obj.turtle_change.Connect()
+
+
+########subscription mode
 sub=RRN.SubscribeService(url)
 while True:
-   try:
-       obj = sub.GetDefaultClient()
-       turtle_change=sub.SubscribeWire("turtle_change")
-       break
-   except RR.ConnectionException:
-       time.sleep(0.1)
+	 try:
+			 obj = sub.GetDefaultClient()
+			 turtle_change=sub.SubscribeWire("turtle_change")
+			 break
+	 except RR.ConnectionException:
+			 time.sleep(0.1)
 
 while True:
 	if turtle_change.TryGetInValue()[0]:
